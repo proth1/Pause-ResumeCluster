a3780e16a77045d588fa6f3b608d36cfe41689fb
Phil Andrews <philandrews@Phils-MacBook-Pro.local>
2021-11-15 14:57:57 -0500

    add remaining images and flow diagram

--- pause-resume/pause-resume.go
+++ pause-resume/pause-resume.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"fmt"
 
@@ -21,7 +22,7 @@ var client = lambda.New(session.New())
 
 func getSecret() (string, error) {
 	secretName := "CastAI-API"
-	region := "us-west-2"
+	region := os.Getenv("AWS_REGION")
 
 	//Create a Secrets Manager client
 	svc := secretsmanager.New(session.New(),
@@ -40,28 +41,28 @@ func getSecret() (string, error) {
 			switch aerr.Code() {
 			case secretsmanager.ErrCodeDecryptionFailure:
 				// Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-				fmt.Println(secretsmanager.ErrCodeDecryptionFailure, aerr.Error())
+				log.Println(secretsmanager.ErrCodeDecryptionFailure, aerr.Error())
 
 			case secretsmanager.ErrCodeInternalServiceError:
 				// An error occurred on the server side.
-				fmt.Println(secretsmanager.ErrCodeInternalServiceError, aerr.Error())
+				log.Println(secretsmanager.ErrCodeInternalServiceError, aerr.Error())
 
 			case secretsmanager.ErrCodeInvalidParameterException:
 				// You provided an invalid value for a parameter.
-				fmt.Println(secretsmanager.ErrCodeInvalidParameterException, aerr.Error())
+				log.Println(secretsmanager.ErrCodeInvalidParameterException, aerr.Error())
 
 			case secretsmanager.ErrCodeInvalidRequestException:
 				// You provided a parameter value that is not valid for the current state of the resource.
-				fmt.Println(secretsmanager.ErrCodeInvalidRequestException, aerr.Error())
+				log.Println(secretsmanager.ErrCodeInvalidRequestException, aerr.Error())
 
 			case secretsmanager.ErrCodeResourceNotFoundException:
 				// We can't find the resource that you asked for.
-				fmt.Println(secretsmanager.ErrCodeResourceNotFoundException, aerr.Error())
+				log.Println(secretsmanager.ErrCodeResourceNotFoundException, aerr.Error())
 			}
 		} else {
 			// Print the error, cast err to awserr.Error to get the Code and
 			// Message from an error.
-			fmt.Println(err.Error())
+			log.Println(err.Error())
 		}
 		return "", err
 	}
@@ -77,39 +78,47 @@ func getSecret() (string, error) {
 	var secretStruct Secret
 	err = json.Unmarshal([]byte(secretString), &secretStruct)
 	if err != nil {
+		// If unmarshalling fails, return the error message.
 		return "", err
 	}
 
+	// return the secret to be used for the CastAI API Calls
 	return secretStruct.CastAIKey, nil
 }
 
 func clusterAction(secret, clusterID, action string) error {
-
+	// Define the CastAI API endpoint to be used for the calls.
 	URL := fmt.Sprintf("https://api.cast.ai/v1/kubernetes/external-clusters/%s/%s", clusterID, action)
 	httpClient := &http.Client{}
+
+	// Create the Post request to CastAI to pause or resume the cluster.
 	req, _ := http.NewRequest("POST", URL, nil)
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("X-API-Key", secret)
-	res, err := httpClient.Do(req)
+
+	// Submit the request and capture the response
+	_, err := httpClient.Do(req)
 	if err != nil {
+		// If the API request fails, return the error message
 		return err
 	}
-	defer res.Body.Close()
-	_, err = ioutil.ReadAll(res.Body)
-	if err != nil {
-		return err
-	}
+
 	return nil
 }
 
+// getClusterID will retrieve the list of clusters and create a struct to be used for gathering Cluster ID's.
 func getClusterID(secret string) (ClusterItems, error) {
+
 	var clusters ClusterItems
-	clusterURL := fmt.Sprintf("https://api.cast.ai/v1/kubernetes/external-clusters")
+	clusterURL := "https://api.cast.ai/v1/kubernetes/external-clusters"
 	httpClient := &http.Client{}
 	req, _ := http.NewRequest("GET", clusterURL, nil)
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("X-API-Key", secret)
+
+	// Submit the GET request to retrieve the list of clusters
 	res, err := httpClient.Do(req)
+
 	if err != nil {
 		return clusters, err
 	}
@@ -144,6 +153,7 @@ func handleRequest(ctx context.Context, event json.RawMessage) (string, error) {
 		return "ERROR", err
 	}
 
+	// Iterate through clusters, match the names to the cluster names passed in the EventBridge action.
 	for _, e := range clusters.Items {
 		for _, c := range clustersToAction.ClusterNames {
 			if c == e.Name {
@@ -152,7 +162,7 @@ func handleRequest(ctx context.Context, event json.RawMessage) (string, error) {
 		}
 	}
 
-	return secret, nil
+	return "", nil
 }
 
 func main() {
